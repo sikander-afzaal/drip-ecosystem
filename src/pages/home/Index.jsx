13120962deeffe80943d25cdeb1176e0da0e335c
sikander-afzaal <sikander.aaa@gmail.com>
2023-04-28 23:29:39 +0500

    first

---
+++ src/pages/home/Index.jsx
@@ -0,0 +1,11 @@
+import HomeHeader from "../../layout/HomeHeader";
+
+const Home = () => {
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        body: "url('/body-bg.png')",
+      },
+      colors: {
+        primary: "#E5F0FF",
+        neutral: "#353A40",
+        button: "#69B7E1",
+      },
+      boxShadow: {
+        blueShadow: "0px 4px 12px rgba(105, 183, 225, 0.5)",
+      },
+    },
+  },
+  plugins: [],
+};
